f39cda5c4cf096fe5f1d79400e07c71f567b6485
Jarod Luebbert <jarod.luebbert@gmail.com>
2010-04-17 21:40:34 -0500

    Created a GameObject class that the ball and paddle can inherit from.

--- Classes/Ball.h
+++ Classes/Ball.h
@@ -6,27 +6,9 @@
 //  Copyright 2010 nil. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import <CoreGraphics/CoreGraphics.h>
+#import "GameObject.h"
 
-@interface Ball : NSObject {
-  int x;
-  int y;
-  int x_velocity;
-  int y_velocity;
+@interface Ball : GameObject {
 }
-
-// Not sure if the 'nonatomic' keyword is the one to use, but it works.
-// If this is a problem, please fix. ;)
-// - Jarod
-@property (nonatomic) int x;
-@property (nonatomic) int y;
-
-- (void) Render;
-- (void) Update;
-- (void) moveY:(int) dir;
-- (void) moveX:(int) dir;
-- (void) setPos:(CGPoint) p;
 
 @end
